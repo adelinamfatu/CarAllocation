62830c4f3472e4db75983c9970c367e20a45884e
roxyenescu <115460507+roxyenescu@users.noreply.github.com>
2025-01-06 01:06:15 +0200

    Implemented UserHandler

---
+++ src/main/java/org/car/allocation/handler/UserHandler.java
@@ -0,0 +1,76 @@
+package org.car.allocation.handler;
+
+import org.car.allocation.model.User;
+import org.car.allocation.service.UserService;
+import org.car.allocation.util.UserRole;
+import java.util.Scanner;
+import java.text.MessageFormat;
+import java.util.ResourceBundle;
+
+public class UserHandler {
+    private final Scanner scanner;
+    private final UserService userService;
+    private static final ResourceBundle messages = ResourceBundle.getBundle("messages");
+
+    public UserHandler(Scanner scanner) {
+        this.scanner = scanner;
+        this.userService = new UserService();
+    }
+
+    public void handleLogin(UserRole role) {
+        System.out.println(messages.getString("login.prompt"));
+        String username = scanner.nextLine();
+        System.out.println(messages.getString("login.password.prompt"));
+        String password = scanner.nextLine();
+
+        User user = userService.findUserByUsername(username).orElse(null);
+
+        if (user != null && user.getPassword().equals(password) && user.getRole() == role) {
+            System.out.println(MessageFormat.format(messages.getString("welcome.message"), username, role));
+            showPermissions(role);
+            new MenuHandler(scanner, role).showOptions();
+        } else {
+            System.out.println(messages.getString("login.invalid"));
+        }
+    }
+
+    public void handleSignIn(UserRole role) {
+        System.out.println(messages.getString("signin.prompt"));
+
+        System.out.println(messages.getString("signin.firstname"));
+        String firstName = scanner.nextLine();
+
+        System.out.println(messages.getString("signin.lastname"));
+        String lastName = scanner.nextLine();
+
+        System.out.println(messages.getString("signin.email"));
+        String email = scanner.nextLine();
+
+        System.out.println(messages.getString("signin.phone_number"));
+        String phoneNumber = scanner.nextLine();
+
+        System.out.println(messages.getString("login.prompt"));
+        String username = scanner.nextLine();
+
+        System.out.println(messages.getString("login.password.prompt"));
+        String password = scanner.nextLine();
+
+        userService.createUser(firstName, lastName, email, phoneNumber, username, password, role);
+
+        System.out.println(messages.getString("signin.successful"));
+    }
+
+    private void showPermissions(UserRole role) {
+        switch (role) {
+            case DRIVER:
+                System.out.println(messages.getString("permissions.driver"));
+                break;
+            case MANAGER:
+                System.out.println(messages.getString("permissions.manager"));
+                break;
+            case ADMIN:
+                System.out.println(messages.getString("permissions.admin"));
+                break;
+        }
+    }
+}
